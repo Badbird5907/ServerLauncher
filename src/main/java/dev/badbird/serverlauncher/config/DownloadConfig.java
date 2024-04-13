89c832db31756333f564ff739292b10e3285ea8c
Pylons <ngedwards998@gmail.com>
2024-04-13 13:49:20 -0700

    downloader would always throw Source isnt set? added else to not do this anymore

--- src/main/java/dev/badbird/serverlauncher/config/DownloadConfig.java
+++ src/main/java/dev/badbird/serverlauncher/config/DownloadConfig.java
@@ -15,18 +15,25 @@ public class DownloadConfig {
     private GithubFileSource githubFile;
 
     public void download(File file) {
-        if (jenkins != null) jenkins.preDownload(file);
-        else if (teamCity != null) teamCity.preDownload(file);
-        else if (github != null) github.preDownload(file);
-        else if (githubFile != null) githubFile.preDownload(file);
-        else if (url != null) url.preDownload(file);
-        System.out.println("[Downloader] Source isn't set, cannot download");
-        //else throw new RuntimeException("Source is not set!");
+        if (jenkins != null)
+            jenkins.preDownload(file);
+        else if (teamCity != null)
+            teamCity.preDownload(file);
+        else if (github != null)
+            github.preDownload(file);
+        else if (githubFile != null)
+            githubFile.preDownload(file);
+        else if (url != null)
+            url.preDownload(file);
+        else
+            System.out.println("[Downloader] Source isn't set, cannot download");
+        // else throw new RuntimeException("Source is not set!");
     }
 
     public void download() {
         if (fileName.isEmpty())
-            throw new RuntimeException("File name is not set! If you want to download a directory from github, set it to '.', or the directory name");
+            throw new RuntimeException(
+                    "File name is not set! If you want to download a directory from github, set it to '.', or the directory name");
         download(new File(fileName));
     }
 }
